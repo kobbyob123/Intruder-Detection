66222091ac23d13598f0d1c91e05d9f8c2cf13a6
kobbyob123 <popolapii@gmail.com>
2026-04-24 13:57:14 +0200

    fixed G and B swap error

--- utils/rgb_2_gray.m
+++ utils/rgb_2_gray.m
@@ -1,8 +1,8 @@
 function newI = rgb_2_gray(I)
     %[m, n, channels] = size(I);
     R = I(:,:,1);
-    B = I(:,:,2);
-    G = I(:,:,3);
+    G = I(:,:,2);
+    B = I(:,:,3);
     
     % ITU-R BT.601 standard
     newI = 0.298936021293775 * R + 0.587043074451121 * G + 0.114020904255103 * B;
