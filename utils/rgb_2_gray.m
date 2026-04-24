a3aaf38dcafa4946d1e43a6c35c01cdbf8eaf7c4
kobbyob123 <popolapii@gmail.com>
2026-04-24 13:42:02 +0200

    finished function

--- utils/rgb_2_gray.m
+++ utils/rgb_2_gray.m
@@ -1,8 +1,9 @@
-function output = rgb_2_gray(image)
-    [m, n, channels] = size(image);
-    if channels == 3
-        % something here
-    end
+function newI = rgb_2_gray(I)
+    [m, n, channels] = size(I);
+    R = I(:,:,1);
+    B = I(:,:,2);
+    G = I(:,:,3);
+    
+    % ITU-R BT.601 standard
+    newI = 0.298936021293775 * R + 0.587043074451121 * G + 0.114020904255103 * B
 end
-
-% 0.298936021293775 * R + 0.587043074451121 * G + 0.114020904255103 * B
