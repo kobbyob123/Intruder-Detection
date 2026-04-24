3889ddac4c9767d773a4d06ec19007278f000522
Kobby <popolapii@gmail.com>
2026-04-24 13:03:52 +0200

    added test file and rgb_2_gray custom file

---
+++ utils/rgb_2_gray.m
@@ -0,0 +1,8 @@
+function output = rgb_2_gray(image)
+    [m, n, channels] = size(image);
+    if channels == 3
+        % something here
+    end
+end
+
+% 0.298936021293775 * R + 0.587043074451121 * G + 0.114020904255103 * B
