487b989d85a4b3f3bc2836d98241a379a2a9e2d9
Kobby <popolapii@gmail.com>
2026-04-27 01:02:42 +0200

    fixed morph_clean

--- Matlab/utils/morph_clean.m
+++ Matlab/utils/morph_clean.m
@@ -1,4 +1,4 @@
 function out = morph_clean(bw, se)
-    opened = dilate(erode(bw, se), se);   % remove speckles
-    out    = erode(dilate(opened, se), se); % fill holes
+    opened = mdilate(merode(bw, se), se);   % remove speckles
+    out    = merode(mdilate(opened, se), se); % fill holes
 end
